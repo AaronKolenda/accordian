ca4a937c733cec062c59eaf974a38a9cb8645ede
AaronKolenda <kolenda.a@gmail.com>
2015-02-10 16:18:22 -0500

    creating borderReset() for proper borders

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ $(document).on("ready", function(){
 		$("#full3").removeClass("full");
 		$("#full1").addClass("full");
 		console.log("header1 was clicked");
+		borderReset();
 
 	});
 
@@ -17,6 +18,7 @@ $(document).on("ready", function(){
 		$("#full2").addClass("full");
 		
 		console.log("header2 was clicked");
+		borderReset();
 
 	});
 
@@ -24,11 +26,55 @@ $(document).on("ready", function(){
 	
 		$("#full1").removeClass("full");
 		$("#full2").removeClass("full");
-		$("#full3").removeClass("full");
+		$("#full3").addClass("full");
 		
 		console.log("header3 was clicked");
+		borderReset();
 
 	});
+
+	var $section1 = $("#full1");
+	var $section2 = $("#full2");
+	var $section3 = $("#full3");
+
+	var $header1 = $("#header1");
+	var $header2 = $("#header2");
+	var $header3 = $("#header3");
+
+	var borderReset = function() {
+
+		$header1.css("border-top", "none");
+		$header2.css("border-top", "none");
+		$header3.css("border-top", "none");
+		$header1.css("border-bottom", "1px solid black");
+		$header2.css("border-bottom", "1px solid black");
+		$header3.css("border-bottom", "1px solid black");
+
+		
+
+		if ($section1.hasClass("full") === true) {
+			$header1.css("border-top", "none");
+			$header2.css("border-top", "1px solid black");
+			$header3.css("border-bottom", "none");
+			$header3.css("border-top", "none");
+		}
+
+		if ($section2.hasClass("full") === true) {
+			$header1.css("border-top", "none");
+			$header1.css("border-bottom", "1px solid black");
+			$header3.css("border-top", "1px solid black");
+			$header3.css("border-bottom", "none");
+		}
+
+		if ($section3.hasClass("full") === true) {
+			$header3.css("border-top", "none");
+			$header2.css("border-top", "none");
+			$header2.css("border-top", "none");
+		}
+
+	}
+
+
 
 });
 
